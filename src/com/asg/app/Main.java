7901b2d76b298468c8a6b69957a587cbb67dc181
ruleless-guy <khun.dev.git@gmail.com>
2020-10-23 20:16:18 +0630

    split mountain pattern to method

--- src/com/asg/app/Main.java
+++ src/com/asg/app/Main.java
@@ -2,7 +2,12 @@ package com.asg.app;
 
 public class Main {
 	public static void main(String[] args) {
-		int num = 10;
+		int num = 8;
+		showMountainPattern(num);
+		
+	}
+	
+	private static void showMountainPattern(int num) {
 		for(int i = 1; i<=num; i++) {
 			for(int j = 1; j <= num-i; j++) {
 				System.out.print(" ");
